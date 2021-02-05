0b54fe0a32de44c0eb9bc23a77d73655447e9d2b
Dhanush Suryavamshi <suryavamshi221b@gmail.com>
2021-02-05 23:25:05 +0530

    Second Commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='blog_home'),
+    path('about/', views.about, name='blog_about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>This sucks</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>This is about page</h1>')
